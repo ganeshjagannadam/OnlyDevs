03dc451dae4e43905210e5f8b63da28b5a21a32f
SasikumarPeddakotla <peddakotlasasikumar@gmail.com>
2024-09-23 12:44:19 +0530

    adding colleges to home page

--- index.js
+++ index.js
@@ -9,12 +9,27 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+
 // Middleware to ignore requests for favicon.ico
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
-app.use('/', (req, res) => {
-  const url = req.url.slice(1);  // Remove the leading '/'
-  req.pipe(request(url)).pipe(res);
+
+const colleges = [
+    "JNTUA College of Engineering, Ananthapuramu",
+    "JNTUA College of Engineering Pulivendula",
+    "Sree Vidyanikethan Engineering College - [SVEC],Tirupati",
+    "Sri Venkateswara University, Tirupati",
+    "G.Pulla Reddy Engineering College, Kurnool"
+]
+
+// app.use('/', (req, res) => {
+//   const url = req.url.slice(1);  // Remove the leading '/'
+//   req.pipe(request(url)).pipe(res);
+// });
+
+app.get('/',(req,res) => {
+    res.render('index', { colleges });
 });
 
 app.listen(8080, () => {
