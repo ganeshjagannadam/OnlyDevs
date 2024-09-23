1b859077c0fc2331bac71e28084fef10ea4664b8
SasikumarPeddakotla <peddakotlasasikumar@gmail.com>
2024-09-23 22:01:04 +0530

    Design of home page

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const request = require('request');
 const path = require('path');
+var bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
@@ -23,6 +25,14 @@ const colleges = [
     "G.Pulla Reddy Engineering College, Kurnool"
 ]
 
+const students = [
+    ["11","12","13"],
+    ["21","22","23"],
+    ["31","32","33"],
+    ["41","42","43"],
+    ["51","52","53"]
+]
+
 // app.use('/', (req, res) => {
 //   const url = req.url.slice(1);  // Remove the leading '/'
 //   req.pipe(request(url)).pipe(res);
@@ -30,6 +40,11 @@ const colleges = [
 
 app.get('/',(req,res) => {
     res.render('index', { colleges });
+});
+
+app.post('/students',(req,res) => {
+    var college = Number(req.body.college);
+    res.render('students',{students : students[college]})
 });
 
 app.listen(8080, () => {
